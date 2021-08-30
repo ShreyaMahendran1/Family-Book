35c6e179c2791b466db23208e7bc292586ef9a6b
ShreyaMahendran1 <84270576+ShreyaMahendran1@users.noreply.github.com>
2021-08-30 15:02:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Family Book","Ganpat Mahendran", "Shreya Mahendran", "Jitha Sivikumaran", "Nikesh Mahendran"];
+var names = ["Family Book", "Nikesh Mahendran", "Ganpat Mahendran", "Shreya Mahendran", "Jitha Sivikumaran", ];
 var i = 0;
 function update()
 {
     i = i+1;
-    var numbers_of_family_member_in_array = 5
+    var numbers_of_family_member_in_array = 4
     if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
-    
+{
+    i=0
+}
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [images];
+    var updatedImage = images[i];
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [names] ;         
+    var updatedName = names[i] ;         
  
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
